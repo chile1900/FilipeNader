35cb5a794a3de58d6d8cbd350ada4f749818afa9
chile1900 <naderfilipe@gmail.com>
2018-11-26 15:28:18 -0200

    cc
    cc

---
+++ assets/js/colapse.js
@@ -0,0 +1,8 @@
+//Hamburger menu toggle
+  $(".navbar-nav li a").click(function (event) {
+    // check if window is small enough so dropdown is created
+    var toggle = $(".navbar-toggle").is(":visible");
+    if (toggle) {
+      $(".navbar-collapse").collapse('hide');
+    }
+  });
